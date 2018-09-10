beb18f86454834b0f34597dbdac1a8831e1d8d6f
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-10 11:34:32 -0400

    update

--- register.php
+++ register.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     
    <form method="post" action="">
         <input type="text" name="username"><br>
-        <input type="password" name="username"><br>
+        <input type="password" name="password"><br>
         <input type="submit">
 
    </form> 
