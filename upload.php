b664c4ad27f1f4f32f9dd30327b1db8a79364b88
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-13 10:12:28 -0400

    update upload

--- upload.php
+++ upload.php
@@ -14,8 +14,8 @@
 
 if (isset($_FILES['upload'])) {
     $target_dir = "uploads/";
-    $target_file = $target_dir . basename($_FILES['upload'] ['name']);
-    move_uploaded_file($_FILES["upload"] ["tmp_name"], $target_file]);
+    $target_file = $target_dir . basename($_FILES['upload']['name']);
+    move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file]);
 
 }
 
