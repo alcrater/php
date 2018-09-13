f539853a74d911a9d16824550edf87527a5e842d
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-13 10:30:41 -0400

    working upload file

--- upload.php
+++ upload.php
@@ -12,12 +12,12 @@
     // echo "<hr />";
     // var_dump($_FILES['upload']);
 
-// if (isset($_FILES['upload'])) {
-//     $target_dir = "uploads/";
-//     $target_file = $target_dir . basename($_FILES['upload']['name']);
-//     move_uploaded_file($_FILES['upload']['tmp_name'], $target_file]);
+if (isset($_FILES['upload'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES['upload']['name']);
+    move_uploaded_file($_FILES['upload']['tmp_name'], $target_file);
 
-// }
+}
 
  ?>
 
