77d711716f4473aa62d89d7b4baf8364b17cad7e
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-18 10:34:59 -0400

    update login

--- login.php
+++ login.php
@@ -3,70 +3,70 @@ session_start();
 require('dbconnection.php');
 
 if (isset($_POST['username'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-     header
-  
-    //SQL statement to execute. surround variables with single qoates
-    $sql = "SELECT username, password FROM users where username = '$username'";
-    //execute sql and return the array to $result
-    $result = $conn->query($sql);
-  
-    //extracting the returned query information
-    while ($row = $result->fetch_assoc()){
-      // $row['username'] is value from database
-      //username & password is the field name in database, use same name and capitalization
-      if ($username == $row['username'] && password_verify($password, $row['password']) ){
-        $_SESSION['username'] = $username;
-             } //closes if statement
-  
-    } //closes while loop
-  
-  }// closes POST condition
-   ?>
-  
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-  
-  <?php
-  
-  if(isset($_POST['logout'])) {
-    unset($_SESSION['username']);
-  }
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-  
+  //SQL statement to execute. surround variables with single qoates
+  $sql = "SELECT username, password FROM users where username = '$username'";
+  //execute sql and return the array to $result
+  $result = $conn->query($sql);
 
-   ?>
-  
-    <body>
+  //extracting the returned query information
+  while ($row = $result->fetch_assoc()){
+    // $row['username'] is value from database
+    //username & password is the field name in database, use same name and capitalization
+    if ($username == $row['username'] && password_verify($password, $row['password']) ){
+      $_SESSION['username'] = $username;
+    } //closes if statement
 
-    <a href="register.php">Register</a>
+  } //closes while loop
 
+}// closes POST condition
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+<?php
+
+if(isset($_POST['logout'])) {
+  unset($_SESSION['username']);
+}
+ ?>
+
+
+
+  <body>
+
+    <a href = "register.php">Register</a>
     <?php
-    if( isset($_SESSION['username'])) {
-   echo "<a href=\"upload.php\"> | Upload</a>";
-    }
+   if (isset($_SESSION['username'])) {
+    echo "<a href =\"upload.php\"> | Upload </a>";
+   }
 
-  ?>
+     ?>
+
+
     <br />
 
-      <form method="post" action="">
-        <input type="text" name="username" placeholder="enter username"> <br />
-        <input type="password" name="password" >
-        <br>
-        <input type="submit" value="go">
-        <br>
-        <input type="submit" name="logout" value="logout">
-  
-      </form>
-  
-  <?php
-  echo "Logged in as: " . $_SESSION['username'];
-   ?>
-  
-    </body>
-  </html>
+
+    <form method="post" action="">
+      <input type="text" name="username" placeholder="enter username"> <br />
+      <input type="password" name="password" >
+      <br>
+      <input type="submit" value="go">
+      <br>
+      <input type="submit" name="logout" value="logout">
+
+    </form>
+
+<?php
+echo "Logged in as: " . $_SESSION['username'];
+ ?>
+
+  </body>
+</html>
