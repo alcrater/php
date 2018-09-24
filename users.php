09a037e0683f982487555f3fc02bea42dc117ea6
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-24 11:25:24 -0400

    update

--- users.php
+++ users.php
@@ -10,7 +10,12 @@ if (!isset($_SESSION['username'])) {
 
 //bring in database connections
 //remember if your connection page is named different change
-require('dbConnect.php');
+require('dbconnection.php');
+
+if (isset($_POST['id']) && isset($_POST['delete'])) {
+  $sql = "DELETE FROM users WHERE userid =" . $_POST['userid'];
+  $result = $conn->query($sql);  
+}
 
 //create the sql Query
 $sql = "SELECT * from users;";
@@ -19,36 +24,44 @@ $sql = "SELECT * from users;";
 $result = $conn->query($sql);
 //close db connection
 $conn->close();
-
-
  ?>
 
-
-<<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
 
 <table>
 <tr>
-<th>User Id</th> 
-<th>Username</th>
-<th>Password Hash</th>
-<th>Actions</th>
-</tr>
+  <!--key names-->
+  <th>User id </th>
+  <th>Username </th>
+  <th>Password Hash</th>
+  <th>Actions</th>
+<tr>
 
 <?php
-//Loop through all table records
+//using \ before " makes it read a single ', but in html will read as "
+//loop through all table records
+while($row = $result->fetch_assoc()){
+  echo "<tr>";
+  echo "<td>" . $row['userid'] . "</td>";
+  echo "<td>" . $row['username'] . "</td>";
+  echo "<td>" . $row['password'] . "</td>";
+  echo "<td>
+  <form action=\"\" method=\"post\">
+    <input name = \"id\" type=\"hidden\" value=\"" . $row['userid'] . "\">
+    <input type =\"submit\" value=\"delete\" name=\"delete\">
+  </form>  </td>";
 
+
+  echo "</tr>";
+}
+?>
 </table>
-    
-</body>
-</html>
 
+</body>
+ </html>
