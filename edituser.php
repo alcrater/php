d2fffb0ccbbb3a17f9931b5c8fcd63c793aae9b9
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-25 10:34:34 -0400

    update while

--- edituser.php
+++ edituser.php
@@ -17,7 +17,7 @@ if (isset($_GET['id']) && $_GET['edit']=="edit"){
     echo "<form action=\"\" method=\"post\">";
     
     while ($row = $result->fetch_assoc()) {
-        echo "<input type=\"text\" disabled value =\"". $row['userid']\">"
+        echo "<input type=\"text\" disabled value =\"". $row['userid'] ."\">"
     }
 
 } else {
