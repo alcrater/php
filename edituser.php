17d7779a57bc26a44c8b152481bd61b7f739794b
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-25 10:31:35 -0400

    edituserrow

--- edituser.php
+++ edituser.php
@@ -8,5 +8,19 @@ if (!isset($_SESSION['username'])) {
    header('Location: login.php');
 }
 
+if (isset($_GET['id']) && $_GET['edit']=="edit"){
+  
+    require('dbconnection.php'); //bring in the database connection
+    $sql = "SELECT * from users where userid =" . $_GET['id']; // id is int datatype note don't quote it
+    $result = $conn->query($sql);
 
-?>
+    echo "<form action=\"\" method=\"post\">";
+    
+    while ($row = $result->fetch_assoc()) {
+        echo "<input type=\"text\" disabled value =\"$row[userid]\">"
+    }
+
+} else {
+    echo "You should not be here.";
+}
+
