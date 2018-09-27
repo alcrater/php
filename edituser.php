052e9a871293b1ff54b022b0267e7f247a919ae5
alcrater <42805962+alcrater@users.noreply.github.com>
2018-09-27 10:24:43 -0400

    add in hidden field and capture information

--- edituser.php
+++ edituser.php
@@ -12,24 +12,17 @@ if (!isset($_SESSION)){
     
     require('dbconnection.php');
     
-    $userid = $_POST['userid'];
-    
-  	$username = $_POST['username'];
-  
-	  $password = $_POST['password'];
-	
-  	$sql = "UPDATE users SET username='$username', password ='$password', WHERE userid = '$userid'";
-
-  	$conn->query($sql);//update
-
-	}//end if post
+    $password = password_hash($password, PASSWORD_BCRYPT);
+  $sql ="UPDATE users set username = '" . $_POST['username'] . "', password = '". $_POST['password'] ."' where userid = '" . $_POST['userid'] . "'";
+  $conn->query($sql);
+}
 
   if (isset($_GET['id']) && $_GET['edit']=="edit") {
     require('dbconnection.php'); //bring in database connection
     $sql = "SELECT * from users where userid = " . $_GET['id']; //id is int datatype don't quote it
     $result = $conn->query($sql);
   
-    echo "<form action=\"\" method=\"get\">";
+    echo "<form action=\"\" method=\"post\">";
   
     while ($row = $result->fetch_assoc()) {
       echo "<input name=\"userid\" type=\"text\" disabled value=\"" . $row['userid'] . "\">";
