653a7504ed10eccd47bfa6bd8916561b742094fe
alcrater <42805962+alcrater@users.noreply.github.com>
2018-10-01 21:22:56 -0400

    navchange

--- navbar.php
+++ navbar.php
@@ -1,27 +1,27 @@
 <?php
 
-if (basename($_SERVER['PHP_SELF']) == 'users.php') {
-    echo "<a href='users.php'><strong>Users</strong></a>";
+// if (basename($_SERVER['PHP_SELF']) == 'users.php') {
+//     echo "<a href='users.php'><strong>Users</strong></a>";
     
-}else {
-    echo "<a href=uploads.php><strong>|Uploads</strong></a>";
-    echo "<a href=login.php')<strong>|Login</strong></a>";
-    echo "<a href=register.php')<strong>|Register</strong></a>";
+// }else {
+//     echo "<a href=uploads.php><strong>|Uploads</strong></a>";
+//     echo "<a href=login.php')<strong>|Login</strong></a>";
+//     echo "<a href=register.php')<strong>|Register</strong></a>";
     
-} 
+// } 
     
     
 
 
-// echo (basename($_SERVER['PHP_SELF']) == 'users.php') ? "<a href=users.php><strong>| Users |</strong></a>" : "<a href=users.php>| Users |</a>";
+echo (basename($_SERVER['PHP_SELF']) == 'users.php') ? "<a href=users.php><strong>| Users |</strong></a>" : "<a href=users.php>| Users |</a>";
 
 
 
-// echo (basename($_SERVER['PHP_SELF']) == 'upload.php') ? "<a href=upload.php><strong> Upload | </strong></a>" : "<a href=upload.php> Upload |</a>";
+echo (basename($_SERVER['PHP_SELF']) == 'upload.php') ? "<a href=upload.php><strong> Upload | </strong></a>" : "<a href=upload.php> Upload |</a>";
 
 
 
-// echo (basename($_SERVER['PHP_SELF']) == 'login.php') ? "<a href=login.php><strong> Login | </strong></a>" : "<a href=login.php> Login |</a>";
+echo (basename($_SERVER['PHP_SELF']) == 'login.php') ? "<a href=login.php><strong> Login | </strong></a>" : "<a href=login.php> Login |</a>";
 
 
 
