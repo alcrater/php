4242a64b35b851775672c59559950aaa468afc92
alcrater <42805962+alcrater@users.noreply.github.com>
2018-10-04 18:55:41 -0400

    update last visit

--- cookies.php
+++ cookies.php
@@ -19,12 +19,14 @@ $cookie_value="bob";
 
     <?php
    //check to see if cookie has already been set 
-        if (isset($_COOKIE['user'])) {
-            echo "You have been here before";
-        } $last = $_COOKIE['user']; }
-        $year = 31536000 + time() ;
-        //this adds one year to the current time, for the cookie expiration
+        if (isset($_COOKIE['user']))
+         {
+         $last = $_COOKIE['user']; }
+
+        $year = 31536000 + time() ; //this adds one year to the current time, for the cookie expiration
+
         setcookie(user, time (), $year) ;
+        
         if (isset ($last))
         {
         $change = time () - $last;
@@ -44,7 +46,7 @@ $cookie_value="bob";
         echo "This is your first visit to this page.";
         //Greets a first time user
         }
-        ?> 
+     ?> 
 
         <!--setcookie($cookie_name, $cookie_value, time() + (60), "/"); // this expires the cookie-->
     ?> 
