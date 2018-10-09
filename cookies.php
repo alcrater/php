bad831ce7ff58d2dfd8e0f8603f02f497fe00371
alcrater <42805962+alcrater@users.noreply.github.com>
2018-10-08 20:41:20 -0400

    test

--- cookies.php
+++ cookies.php
@@ -24,104 +24,37 @@ setcookie($cookie_name, $cookie_date, time()+(84600 * 30), "/");
  <?php
    //check to see if cookie has already been set 
    if (isset($_COOKIE['user'])) {
-    // $last = $_COOKIE['user']; }
+    $last = $_COOKIE['user']; }
 
-    //  $day = 86400 + time() ;
-    //  //this sets day to the current time, for the cookie expiration
-    //  setcookie(user, time (), $day);
+     $year = 31536000 + time() ;
+     //this adds one year to the current time, for the cookie expiration
+     setcookie(user, time (), $year);
 
-    //  if (isset ($last))
-    //  {
-    //  $change = time () - $last;
-    //  if ( $change > 86400)
-    //  {
-    //  echo "Hello Mate!!! <br> Your last visit was ". date("G:i - m/d/y",$last) ;
-    //  // Tells the user when they last visited if it was over a day ago
-    //  }
-    //  else
-    //  {
-    //  echo "Thanks for viewing the page, mate!";
-    //  //Gives the user a message if they are visiting again in the same day
-    //  }
-    //  }
-    //  else
-    //  {
-    //  echo "This is your first visit to this page.";
-    //  //Greets a first time user
-    //   }
-   
-        setcookie($cookie_name, $cookie_value, time() + (60), "/"); // this expires the cookie
+     if (isset ($last))
+     {
+     $change = time () - $last;
+     if ( $change > 86400)
+     {
+     echo "Hello Mate!!! <br> You last visited this page on ". date("m/d/y",$last) ;
+     // Tells the user when they last visited if it was over a day ago
+     }
+     else
+     {
+     echo "Thanks for viewing the page, mate!";
+     //Gives the user a message if they are visiting again in the same day
+     }
+     }
+     else
+     {
+     echo "This is your first visit to this page.";
+     //Greets a first time user
+      }
+?> 
+  
 
-            function timeAgo($time_ago)
-            {
-                $time_ago = strtotime($time_ago);
-                $cur_time   = time();
-                $time_elapsed   = $cur_time - $time_ago;
-                $seconds    = $time_elapsed ;
-                $minutes    = round($time_elapsed / 60 );
-                $hours      = round($time_elapsed / 3600);
-                $days       = round($time_elapsed / 86400 );
-                $weeks      = round($time_elapsed / 604800);
-                $months     = round($time_elapsed / 2600640 );
-                $years      = round($time_elapsed / 31207680 );
-                // Seconds
-                if (isset($_COOKIE['user'])) {
-                }
-                
-                if($seconds <= 60){
-                    return "just now";
-                }
-                //Minutes
-                else if($minutes <=60){
-                    if($minutes==1){
-                        return "one minute ago";
-                    }
-                    else{
-                        return "$minutes minutes ago";
-                    }
-                }
-                //Hours
-                else if($hours <=24){
-                    if($hours==1){
-                        return "an hour ago";
-                    }else{
-                        return "$hours hrs ago";
-                    }
-                }
-                //Days
-                else if($days <= 7){
-                    if($days==1){
-                        return "yesterday";
-                    }else{
-                        return "$days days ago";
-                    }
-                }
-                //Weeks
-                else if($weeks <= 4.3){
-                    if($weeks==1){
-                        return "a week ago";
-                    }else{
-                        return "$weeks weeks ago";
-                    }
-                }
-                //Months
-                else if($months <=12){
-                    if($months==1){
-                        return "a month ago";
-                    }else{
-                        return "$months months ago";
+     <!--setcookie($cookie_name, $cookie_value, time() + (60), "/"); // this expires the cookie-->
 
-                    }
-                }
-                //Years
-                else{
-                    if($years==1){
-                        return "one year ago";
-                    }else{
-                        return "$years years ago";
-                    }
-        }
-    }
+       
 
 
 
