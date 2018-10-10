5d2bd263e555c9c74a68fe059782b62a2aa64fe0
alcrater <42805962+alcrater@users.noreply.github.com>
2018-10-10 11:38:02 -0400

    update

--- shell_exec.php
+++ shell_exec.php
@@ -3,11 +3,11 @@ $output = shell_exec('ls -lah');
 echo "$output";
 
 $pwd = shell_exec('pwd');
-echo ="$pwd";
+echo ="<pre>$pwd</pre>";
 ?>
 
 <?php
-$filename = "/var/www/html/audrey/audrey/php/test";
+$filename = "/var/www/html/audrey/php/test";
 
 if (file_exists($filename)) {
     echo "$filename does exist";
