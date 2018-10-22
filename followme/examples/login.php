9cfa9fd6ae22a2e8c1f46987ab01076265d28717
alcrater <42805962+alcrater@users.noreply.github.com>
2018-10-22 18:57:23 -0400

    updates

--- followme/examples/login.php
+++ followme/examples/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require('dbonnection.php');
+require('dbconnection.php');
 
 if (isset($_POST['email'])){
     $email = $_POST['email'];
@@ -31,7 +31,7 @@ if (isset($_POST['email'])){
     }// closes POST condition
     
     if (isset($_SESSION['email'])) {
-    header('location: profile.php');
+    header('Location: profile.php');
     
     } //closes if (isset)
     
@@ -43,157 +43,236 @@ if (isset($_POST['email'])){
 
 <head>
 
-<meta charset="utf-8" />
+	<meta charset="utf-8" />
 
-<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
 
-<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
 
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-<title>Login</title>
 
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
-<meta name="viewport" content="width=device-width" />
 
-<!-- Bootstrap core CSS -->
+	<title>Paper Kit 2 by Chaos</title>
 
-<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
 
-<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-<!-- CSS for Demo Purpose, don't include it in your project -->
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 
-<link href="../assets/css/demo.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width" />
 
-<!-- Fonts and icons -->
 
-<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
 
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Bootstrap core CSS     -->
 
-<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+
+	<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+
+
+
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+
+	<link href="../assets/css/demo.css" rel="stylesheet" />
+
+
+
+    <!--     Fonts and icons     -->
+
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+	<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
+
 
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-md fixed-top navbar-transparent">
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent">
 
-<div class="container">
+        <div class="container">
 
-<div class="navbar-translate">
+			<div class="navbar-translate">
 
-<button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+	            <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 
-<span class="navbar-toggler-bar"></span>
+					<span class="navbar-toggler-bar"></span>
 
-<span class="navbar-toggler-bar"></span>
+					<span class="navbar-toggler-bar"></span>
 
-<span class="navbar-toggler-bar"></span>
+					<span class="navbar-toggler-bar"></span>
 
-</button>
+	            </button>
 
-<a class="navbar-brand" href="#">login</a>
+	            <a class="navbar-brand" href="https://www.creative-tim.com">Paper Kit 2</a>
 
-</div>
+			</div>
 
-<div class="collapse navbar-collapse" id="navbarToggler">
+			<div class="collapse navbar-collapse" id="navbarToggler">
 
-<ul class="navbar-nav ml-auto">
+	            <ul class="navbar-nav ml-auto">
 
-<li class="nav-item">
+					<li class="nav-item">
 
-<a href="../index.html" class="nav-link"><i class="nc-icon nc-layout-11"></i>Components</a>
+	                    <a href="../index.html" class="nav-link"><i class="nc-icon nc-layout-11"></i>Components</a>
 
-</li>
+	                </li>
 
-</ul>
+	                <li class="nav-item">
 
-</div>
+	                    <a href="../documentation/tutorial-components.html" target="_blank" class="nav-link"><i class="nc-icon nc-book-bookmark"></i>  Documentation</a>
 
-</div>
+	                </li>
 
-</nav>
+					<li class="nav-item">
 
-<div class="wrapper">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
 
-<div class="page-header" style="background-image: url('../assets/img/login-image.jpg');">
+                            <i class="fa fa-twitter"></i>
 
-<div class="filter"></div>
+                            <p class="d-lg-none">Twitter</p>
 
-<div class="container">
+                        </a>
 
-<div class="row">
+                    </li>
 
-<div class="col-lg-4 ml-auto mr-auto">
+                    <li class="nav-item">
 
-<div class="card card-register">
+                        <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
 
-<h3 class="title">Login</h3>
+                            <i class="fa fa-facebook-square"></i>
 
-<div class="social-line text-center">
+                            <p class="d-lg-none">Facebook</p>
 
-<a href="#pablo" class="btn btn-neutral btn-facebook btn-just-icon">
+                        </a>
 
-<i class="fa fa-facebook-square"></i>
+                    </li>
 
-</a>
+                    <li class="nav-item">
 
-<a href="#pablo" class="btn btn-neutral btn-google btn-just-icon">
+                        <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
 
-<i class="fa fa-google-plus"></i>
+                            <i class="fa fa-instagram"></i>
 
-</a>
+                            <p class="d-lg-none">Instagram</p>
 
-<a href="#pablo" class="btn btn-neutral btn-twitter btn-just-icon">
+                        </a>
 
-<i class="fa fa-twitter"></i>
+                    </li>
 
-</a>
+                    <li class="nav-item">
 
-</div>
+                        <a class="nav-link" rel="tooltip" title="Star on GitHub" data-placement="bottom" href="https://www.github.com/CreativeTimOfficial" target="_blank">
 
-<form class="register-form" method="post" action="">
+                            <i class="fa fa-github"></i>
 
-<label>Email</label>
+                            <p class="d-lg-none">GitHub</p>
 
-<input type="text" class="form-control" name="email" placeholder="Email">
+                        </a>
 
-<label>Password</label>
+                    </li>
 
-<input type="password" class="form-control" name="password" placeholder="Password">
+	            </ul>
 
-<button class="btn btn-danger btn-block btn-round">Login</button>
+	        </div>
 
-</form>
+		</div>
 
-<div class="forgot">
+    </nav>
 
-<a href="#" class="btn btn-link btn-danger">Forgot password?</a>
+    <div class="wrapper">
 
-</div>
+        <div class="page-header" style="background-image: url('../assets/img/login-image.jpg');">
 
-</div>
+            <div class="filter"></div>
 
-</div>
+                <div class="container">
 
-</div>
+                    <div class="row">
 
-<div class="footer register-footer text-center">
+                        <div class="col-lg-4 ml-auto mr-auto">
 
-<h6>&copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Choas</h6>
+                            <div class="card card-register">
 
-</div>
+                                <h3 class="title">Login</h3>
 
-</div>
+								<div class="social-line text-center">
 
-</div>
+                                    <a href="#pablo" class="btn btn-neutral btn-facebook btn-just-icon">
 
-</div>
+                                        <i class="fa fa-facebook-square"></i>
+
+                                    </a>
+
+                                    <a href="#pablo" class="btn btn-neutral btn-google btn-just-icon">
+
+                                        <i class="fa fa-google-plus"></i>
+
+                                    </a>
+
+									<a href="#pablo" class="btn btn-neutral btn-twitter btn-just-icon">
+
+										<i class="fa fa-twitter"></i>
+
+									</a>
+
+
+
+
+
+
+
+
+
+                                </div>
+
+                                <form class="register-form" method="post" action="">
+
+                                    <label>Email</label>
+
+                                    <input type="text" class="form-control" name="email" placeholder="Email">
+
+
+
+                                    <label>Password</label>
+
+                                    <input type="password" class="form-control" name="password" placeholder="Password">
+
+                                    <button class="btn btn-danger btn-block btn-round">Login</button>
+
+                                </form>
+
+                                <div class="forgot">
+
+                                    <a href="#" class="btn btn-link btn-danger">Forgot password?</a>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+					<div class="footer register-footer text-center">
+
+						<h6>&copy; <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Choas</h6>
+
+					</div>
+
+                </div>
+
+        </div>
+
+    </div>
 
 </body>
+
+
 
 <!-- Core JS Files -->
 
@@ -205,8 +284,12 @@ if (isset($_POST['email'])){
 
 <script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
 
-<!-- Paper Kit Initialization snd functons -->
+
+
+<!--  Paper Kit Initialization snd functons -->
 
 <script src="../assets/js/paper-kit.js?v=2.0.1"></script>
+
+
 
 </html>
