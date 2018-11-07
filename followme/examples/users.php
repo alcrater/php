2562c0b0bcaef58ad40b07638aa86c4f20475942
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-07 11:31:13 -0500

    update

--- followme/examples/users.php
+++ followme/examples/users.php
@@ -1,68 +1,70 @@
 <?php
-//Start Session if it is not running
-//Add name attributes to form elements
-//Set default values for each form element from $_SESSION
-//Update submitted values to database
-//Upldate submitted values to $_SESSION
-
-
 if (!isset($_SESSION)) {
-  session_start();
+session_start();
 }
-require('dbconnection.php'); //bring in database connection
-
-//for if not logged in
-  if (!isset($_SESSION['email'])){
-    header('location: login.php');
-  }
-
-
-//create the sql Query
-$sql = "SELECT * from fm_users;";
-//exacute the sql query
-$result = $conn->query($sql);
+require('dbconnection.php');
 
 $user_id = $_SESSION['user_id'];
 
+$sql2 = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+$result2 = $conn->query($sql2);
 
-$sql = "SELECT following_user_id FROM fm_followers WHERE user_id = $user_id";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+while ($row2 = $result2->fetch_assoc()) {
+$first_name = $row2['first_name'];
 
-$following_result = $conn->query($sql);
+if ($_POST["$first_name"] == "yes") {
 
-//indexes
-while($row = $following_result->fetch_row()){
-
-  $following_user_id[] = $row[0];
+$follow_id = $row2['user_id'];
+$sql2 = "INSERT IGNORE INTO fm_follows (user_id, following_user_id) VALUES ('$user_id','$follow_id')";
+$conn->query($sql2);
+}
+else {
+$follow_id = $row2['user_id'];
+$sql2 = "DELETE FROM fm_followers WHERE user_id = '$user_id' AND following_user_id = '$follow_id'";
+$conn->query($sql2);
+}
+}
 }
 
+$sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+$result = $conn->query($sql);
+
+$sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$user_id'";
+
+$follow_result = $conn->query($sql);
+
+while($row = $follow_result->fetch_row()) {
+$following_user_id[] = $row[0];
+}
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta charset="utf-8" />
+<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<title>Users by Chaos</title>
+<title>Users</title>
 
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-  <meta name="viewport" content="width=device-width" />
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+<meta name="viewport" content="width=device-width" />
 
-	<!-- Bootstrap core CSS     -->
-	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-	<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+<!-- Bootstrap core CSS -->
+<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-	<!--  CSS for Demo Purpose, don't include it in your project     -->
-	<link href="../assets/css/demo.css" rel="stylesheet" />
+<!-- CSS for Demo Purpose, don't include it in your project -->
+<link href="../assets/css/demo.css" rel="stylesheet" />
 
-    <!--     Fonts and icons     -->
-	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-	<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
-
+<!-- Fonts and icons -->
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
 </head>
+
 <body>
   <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
     <div class="container">
@@ -118,7 +120,7 @@ while($row = $following_result->fetch_row()){
 								<!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 								</div>
 								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
-							<!--name-->		<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
+							<!--name-->		<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
 
 							<!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
 								</div>
@@ -161,3 +163,4 @@ while($row = $following_result->fetch_row()){
                 <div class="credits ml-auto">
                     <span class="copyright">
                         © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
+                        </html>
