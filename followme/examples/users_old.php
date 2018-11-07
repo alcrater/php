3b00c71d39b43239a191347ba03e5c270a1beba7
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-07 12:07:21 -0500

    update

--- followme/examples/users_old.php
+++ followme/examples/users_old.php
@@ -127,11 +127,11 @@ $following_user_id[] = $row[0];
 								</div>
 								<div class="col-md-3 col-sm-2  ml-auto mr-auto">
 									<div class="form-check">
-										<label class="form-check-label"><!--echo if checked only if followed -->
-											<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>"
-                                            value="yes" 
-                                            <?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?> >
-											<span class="form-check-sign"></span>
+								<label class="form-check-label"><!--echo if checked only if followed -->
+								<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>"
+                              value="yes" 
+                              <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+					     		<span class="form-check-sign"></span>
 										</label>
 									</div>
 								</div>
