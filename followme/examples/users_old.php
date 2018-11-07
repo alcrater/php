dd11edc135034b154e2d6c5f0a1d87c8f18cff50
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-07 15:41:39 -0500

    older version

--- followme/examples/users_old.php
+++ followme/examples/users_old.php
@@ -1,82 +1,68 @@
 <?php
-if (!isset($_SESSION)) {
-session_start();
-}
-require('dbconnection.php');
+//Start Session if it is not running
+//Add name attributes to form elements
+//Set default values for each form element from $_SESSION
+//Update submitted values to database
+//Upldate submitted values to $_SESSION
 
+
+if (!isset($_SESSION)) {
+  session_start();
+}
+require('dbconnection.php'); //bring in database connection
 
 //for if not logged in
-if (!isset($_SESSION['email'])){
+  if (!isset($_SESSION['email'])){
     header('location: login.php');
   }
 
-  $userid = $_SESSION['user_id'];
 
-  $sql2 = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
-  $result2 = $conn->query($sql2);
-  
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  
-	while ($row2 = $result2->fetch_assoc()) {
-  
-	  $firstName = $row2['first_name'];
-  
-	  if ($_POST["$firstName"] == "yes") {
-  
-	  $follow_id = $row2['user_id'];
-	  $sql2 = "INSERT IGNORE INTO fm_follows (user_id, following_user_id) VALUES ('$userid','$follow_id')";
-	  $conn->query($sql2);
-	}
-  
-	  else {
-  
-		$follow_id = $row2['user_id'];
-		$sql2 = "DELETE FROM fm_follows WHERE user_id = '$userid' AND following_user_id = '$follow_id'";
-		$conn->query($sql2);
-	  }
-	}
-  }
-  
-  $sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
-  $result = $conn->query($sql);
-  
-  $sql = "SELECT following_user_id FROM fm_follows WHERE user_id = '$userid'";
-  
-  $follow_result = $conn->query($sql);
-  
-  while($row = $follow_result->fetch_row()) {
-  
-	$following_user_ids[] = $row[0];
-  }
-  
-  ?>
+//create the sql Query
+$sql = "SELECT * from fm_users;";
+//exacute the sql query
+$result = $conn->query($sql);
 
-<!DOCTYPE html>
+$user_id = $_SESSION['user_id'];
+
+
+$sql = "SELECT following_user_id FROM fm_follows WHERE fm_user_id = $user_id";
+
+$following_result = $conn->query($sql);
+
+//indexes
+while($row = $following_result->fetch_row()){
+
+  $following_user_id[] = $row[0];
+}
+
+?>
+
+<!doctype html>
 <html lang="en">
 <head>
-<meta charset="utf-8" />
-<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
-<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta charset="utf-8" />
+	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-<title>Users</title>
+	<title>Users</title>
 
-<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-<meta name="viewport" content="width=device-width" />
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+  <meta name="viewport" content="width=device-width" />
 
-<!-- Bootstrap core CSS -->
-<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+	<!-- Bootstrap core CSS     -->
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
 
-<!-- CSS for Demo Purpose, don't include it in your project -->
-<link href="../assets/css/demo.css" rel="stylesheet" />
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+	<link href="../assets/css/demo.css" rel="stylesheet" />
 
-<!-- Fonts and icons -->
-<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
-<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+    <!--     Fonts and icons     -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+	<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
 </head>
-
 <body>
   <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
     <div class="container">
@@ -104,7 +90,7 @@ if (!isset($_SESSION['email'])){
                     </li>
                     <li class="nav-item">
 	                    <a href="#" class="nav-link">
-				<?php echo $_SESSION['email']; ?>
+												<?php echo $_SESSION['email']; ?>
 											</a>
 	                </li>
 	            </ul>
@@ -112,64 +98,45 @@ if (!isset($_SESSION['email'])){
 		</div>
     </nav>
 
-   <div class="wrapper">
+    <div class="wrapper">
       <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-                          <div class="filter"></div>
-                  </div>
+			  <div class="filter"></div>
+		  </div>~
 
-                        <br />
-                        <br />
+			<br />
+			<br />
 
-                        <div class="row">
-                                <div class="col-md-6 ml-auto mr-auto">
-                                        <ul class="list-unstyled follows">
-            <form method="post" action="">
-                                                <?php
+    <form>
+      
+			<div class="row">
+				<div class="col-md-6 ml-auto mr-auto">
+					<ul class="list-unstyled follows">
+						 <?php while($row = $result->fetch_assoc()){ ?>
+						<li>
+							<div class="row">
+								<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+								<!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+								</div>
+								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+							<!--name-->		<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
 
-            while($row = $result->fetch_assoc()) {
-
-              $user_id = $row['user_id'];
-
-              if ($user_id == $userid){
-
-              }
-              else {
-
-              echo "<li>";
-                                                echo    "<div class=\"row\">";
-                                                echo            "<div class=\"col-md-2 col-sm-2 ml-auto mr-auto\">";
-                                                echo                    "<img src=" . $row['image_url'] . " alt=\"Circle Image\" class=\"img-circle img-no-padding img-responsive\">";
-                                                echo            "</div>";
-                                                echo            "<div class=\"col-md-7 col-sm-4  ml-auto mr-auto\">";
-                                                echo                    "<h6>" . $row['first_name'] . " " . $row['last_name'] . "<br/><small>" . $row['title'] . "</small></h6>";
-                                                echo            "</div>";
-                                                echo            "<div class=\"col-md-3 col-sm-2  ml-auto mr-auto\">";
-                                                echo                    "<div class=\"form-check\">";
-                                                echo                            "<label class=\"form-check-label\">";
-                                                echo                                    "<input class=\"form-check-input\" name=" . $row['first_name'] . " type=\"checkbox\" value=\"yes\"";
-
-              if (in_array($user_id, $following_user_ids)) {
-
-                echo " checked";
-              }
-              echo ">";
-                                                echo                                    "<span class=\"form-check-sign\"></span>";
-                                                echo                            "</label>";
-                                                echo                    "</div>";
-                                                echo            "</div>";
-                                                echo    "</div>";
-                                                echo "</li>";
-              echo "<hr />";
-
-              }
-            }
-                                                ?>
-            <input type="submit">
-          </form>
-                                        </ul>
-                                </div>
-                        </div>
-                </div>
+							<!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
+								</div>
+								<div class="col-md-3 col-sm-2  ml-auto mr-auto">
+									<div class="form-check">
+										<label class="form-check-label"><!--echo if checked only if followed -->
+											<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="<?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?>" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+											<span class="form-check-sign"></span>
+										</label>
+									</div>
+								</div>
+							</div>
+						</li>
+						<hr />
+					<?php } ?>
+					</ul>
+				</div>
+			</div>
 
 
       <div class="row">
@@ -194,4 +161,3 @@ if (!isset($_SESSION['email'])){
                 <div class="credits ml-auto">
                     <span class="copyright">
                         © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
-                        </html>
