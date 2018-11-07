e2dea5317209e3ffc1fa3a7e53bc05fe399ddd02
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-07 11:32:32 -0500

    update

--- followme/examples/users_old.php
+++ followme/examples/users_old.php
@@ -4,7 +4,39 @@ session_start();
 }
 require('dbconnection.php');
 
+$user_id = $_SESSION['user_id'];
 
+$sql2 = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+$result2 = $conn->query($sql2);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+while ($row2 = $result2->fetch_assoc()) {
+$first_name = $row2['first_name'];
+
+if ($_POST["$first_name"] == "yes") {
+
+$follow_id = $row2['user_id'];
+$sql2 = "INSERT IGNORE INTO fm_follows (user_id, following_user_id) VALUES ('$user_id','$follow_id')";
+$conn->query($sql2);
+}
+else {
+$follow_id = $row2['user_id'];
+$sql2 = "DELETE FROM fm_followers WHERE user_id = '$user_id' AND following_user_id = '$follow_id'";
+$conn->query($sql2);
+}
+}
+}
+
+$sql = "SELECT user_id, first_name, last_name, title, image_url FROM fm_users";
+$result = $conn->query($sql);
+
+$sql = "SELECT following_user_id FROM fm_followers WHERE user_id = '$user_id'";
+
+$follow_result = $conn->query($sql);
+
+while($row = $follow_result->fetch_row()) {
+$following_user_id[] = $row[0];
+}
 ?>
 
 <!DOCTYPE html>
@@ -68,39 +100,45 @@ require('dbconnection.php');
 		</div>
     </nav>
 
-    <ul class="list-unstyled follows">
-    <?php
-     require('sitedbconn.php');
-     $followingsql = "SELECT * FROM fm_users";
+    <div class="wrapper">
+      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+			  <div class="filter"></div>
+		  </div>~
 
-     $theresult = $conn->query($followingsql);
-     $followers_result=$conn->query($followers);
+			<br />
+			<br />
 
-    while($row = $follows_result->fetch_assoc()){
-    $follow_array[]=$row['follower_user_id'];
-     }
+    <form>
+      
+			<div class="row">
+				<div class="col-md-6 ml-auto mr-auto">
+					<ul class="list-unstyled follows">
+						 <?php while($row = $result->fetch_assoc()){ ?>
+						<li>
+							<div class="row">
+								<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+								<!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+								</div>
+								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+							<!--name-->		<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
 
-                                             while ($row = $theresult->fetch_assoc()) { ?>
-                                                <li>
-                                                        <div class="row">
-                                                                <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-                                                                        <img src="<?php echo $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-                                                                </div>
-                                                                <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                                                                       <h6> <?php echo $row['first_name'];
-                                                                                                                                                                                                                                                                                         echo " ";
-                                                                                                                                                                                                                                                                                         echo $row['last_name']; ?> <br/><small> <?php echo $row['title']; ?>  </small></h6>
-                                                              </div>
-                                                                <div class="col-md-3 col-sm-2  ml-auto mr-auto">
-                                                                        <div class="form-check">
-                                                                                <label class="form-check-label">
-                                                                                        <input class="form-check-input" type="checkbox" value="" <?php if (in_array($row['user_id'], $follow_array)){echo "checked";} ?>>
-                                                                                        <span class="form-check-sign"></span>
-                                                                                </label>
-                                                                        </div>
-                                                                </div>
-                                                        </div>
-                                                </li>
+							<!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
+								</div>
+								<div class="col-md-3 col-sm-2  ml-auto mr-auto">
+									<div class="form-check">
+										<label class="form-check-label"><!--echo if checked only if followed -->
+											<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="<?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?>" <?php if (in_array($row['user_id'], $fm_following_user_id)){echo "checked";}?> >
+											<span class="form-check-sign"></span>
+										</label>
+									</div>
+								</div>
+							</div>
+						</li>
+						<hr />
+					<?php } ?>
+					</ul>
+				</div>
+			</div>
 
 
       <div class="row">
