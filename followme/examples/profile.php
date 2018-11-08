04dca230de01cbe9c6dc2c437cd15b13f646b4cb
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-08 14:43:50 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -173,10 +173,29 @@ $following_user_id[] = $row[0];
 </div>
 </div>
 </div>
-<div class="tab-pane text-center" id="following" role="tabpanel">
-<h3 class="text-muted">Not following anyone yet :(</h3>
-<button class="btn btn-warning btn-round">Find artists</button>
+<div class="tab-pane text-center" id="following" role="tabpanel"><!--following tab-->
+<!--grab from users_old-->
+<?php while($row = $result->fetch_assoc()){ ?>
+<div class="row">
+<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
+<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
+<br/><small><?php echo $row['title'] ; ?></small></h6>
+</div>
+<div class="col-md-3 col-sm-2  ml-auto mr-auto">
+<div class="form-check">
+<label class="form-check-label">
+<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], &following_user_id)){echo "checked";}?> >
+<span class="form-check-sign"></span>
+</label>
+</div>
+</div>
+</div>
+<hr />
+<?php } ?>
+</div><!-- end following tab-->
 </div>
 </div>
 </div>
