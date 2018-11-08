9b4d683480186b5432e598e05100a5bf01d9763a
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-08 15:23:50 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -170,21 +170,30 @@ $following_user_id[] = $row[0];
 </div>
 <!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
-<?php while($row = $result->fetch_row()){ ?>
-<div class="row">
-<img src="<?php echo $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
-<br/><small><?php echo $row['title'] ; ?></small></h6>
-</div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
-<span class="form-check-sign"></span>
-</label>
-</div>
+<?php while($row = $result->fetch_assoc()){ ?>
+						<li>
+							<div class="row">
+								<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+							<!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+								</div>
+								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+							<!--name-->		<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+
+							<!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
+								</div>
+					<div class="col-md-3 col-sm-2  ml-auto mr-auto">
+									<div class="form-check">
+								<label class="form-check-label"><!--echo if checked only if followed -->
+								<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+					     		<span class="form-check-sign"></span>
+										</label>
+									</div>
+								</div>
+							</div>
+						</li>
+						<hr />
+					<?php } ?>
+          </div>
 </div>
 </div>
 <button class="btn btn-warning btn-round"><a href="users_old.php">Follow More</a></button>
