a84b0803e257ef7a6a96115cf14f67eed002b62a
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-08 15:21:05 -0500

    update

--- followme/examples/profiletemp.php
+++ followme/examples/profiletemp.php
@@ -1,23 +1,10 @@
 
 
 <?php
-
+if (!isset($_SESSION)) {
 session_start();
-require('dbconnection.php');
-
-$sql = "SELECT * FROM fm_users";
-
-$result = $conn->query($sql);
-
-$user_id = $_SESSION['user_id'];
-
-$sql = "SELECT following_user_id FROM fm_followers WHERE fm_user_id = '$user_id'";
-
-$follow_result = $conn->query($sql);
-
-while($row = $follow_result->fetch_row()) {
-$following_user_id[] = $row[0];
 }
+require('dbconnection.php');
 
 ?>
 
@@ -168,26 +155,11 @@ $following_user_id[] = $row[0];
 </div>
 </div>
 </div>
-<!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
-<?php while($row = $result->fetch_row()){ ?>
-<div class="row">
-<img src="<?php echo $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6><?php echo $row['first_name'] . $row['last_name'] ; ?>
-<br/><small><?php echo $row['title'] ; ?></small></h6>
-</div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
-<span class="form-check-sign"></span>
-</label>
+<h3 class="text-muted">Not following anyone yet :(</h3>
+<button class="btn btn-warning btn-round">Find artists</button>
 </div>
 </div>
-</div>
-<button class="btn btn-warning btn-round"><a href="users_old.php">Follow More</a></button>
 </div>
 </div>
 </div>
