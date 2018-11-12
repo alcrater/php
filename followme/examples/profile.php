bb1b0d4e76529945a85280da9a978833a7a4b23f
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 16:30:09 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -19,6 +19,18 @@ while($row = $follow_result->fetch_row()) {
 $following_user_id[] = $row[0];
 }
 
+//following me
+
+$sql3 = "SELECT fm_user_id FROM fm_follows WHERE following_user_id = $userID";
+
+$following_me = $conn->query($sql3);
+
+while ($row = $following_me->fetch_row()) {
+
+  $following_me_users[] = $row[0];
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -122,52 +134,53 @@ $following_user_id[] = $row[0];
 </div>
 
 <!-- Tab panes -->
-<div class="tab-content following">
-<div class="tab-pane active" id="follows" role="tabpanel">
-<div class="row">
-<div class="col-md-6 ml-auto mr-auto">
-<ul class="list-unstyled follows">
-<li>
-<div class="row">
-<div class="col-md-2 col-sm-2 ml-auto mr-auto">
-<img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6>Flume<br/><small>Musical Producer</small></h6>
-</div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="" checked>
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
-</div>
-</li>
-<hr />
-<li>
-<div class="row">
-<div class="col-md-2 ml-auto mr-auto ">
-<img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4">
-<h6>Banks<br /><small>Singer</small></h6>
-</div>
-<div class="col-md-3 col-sm-2">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="">
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
-</div>
-</li>
-</ul>
-</div>
-</div>
-</div>
+
+                <div class="tab-content following"><!-- start for both-->
+
+
+
+<div class="tab-pane active" id="follows" role="tabpanel"><!-- start following you-->
+
+
+
+  <?php while($row = $result->fetch_assoc()){
+
+
+
+ if (in_array($row['user_id'], $following_me_users)) {?>
+
+
+
+   <div class="row">
+
+     <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+
+   <img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+
+
+	<h6><?php echo $row['first_name'] . $row['last_name']; ?>
+
+
+
+   <br/><small><?php 	echo $row['title']; ?></small></h6>
+
+     </div>
+
+   </div>
+
+  <hr />
+
+ <?php } ?>
+
+
+
+<?php } ?>
+
+
+
+
+
+</div> <!-- end following you-->
 
 
 
