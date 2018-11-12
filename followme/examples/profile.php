647f8b91f3207c04c4512e8b738fb687c12c3b75
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:30:06 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -177,10 +177,10 @@ $following_user_id[] = $row[0];
 <?php while($row = $result->fetch_assoc()){ 
   if (in_array($row['user_id'], $following_user_id))?>
 <div class="row">
-<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<div class="col-md-2 ml-auto mr-auto ">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
-<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<div class="col-md-7 col-sm-4">
 <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
 <br/><small><?php 	echo $row['title'] ; ?></small></h6>
 </div>
