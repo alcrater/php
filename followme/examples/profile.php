6f15f8b8bc04a48259faa63cf4325c6af715fc39
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:50:04 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -136,7 +136,7 @@ $following_me[] = $row[0];
 
 <div class="tab-pane text-center" id="following" role="tabpanel">
 <?php while($row = $result->fetch_assoc()){ 
-  if (in_array($row['user_id'], $following_me))?>
+  if (in_array($row2['user_id'], $following_me))?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
