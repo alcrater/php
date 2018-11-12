94e50355bad4fec523627ff34e80472bfa3c8570
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 16:35:33 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -134,25 +134,51 @@ $following_me_users[] = $row[0];
 
 <!-- Tab panes -->
 
-<div class="tab-pane text-center" id="following" role="tabpanel">
-<?php while($row = $result->fetch_assoc()){ 
-  if (in_array($row['user_id'], $following_me_users))?>
-<div class="row">
-<div class="col-md-2 col-sm-2 ml-auto mr-auto">
-<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4  ml-auto mr-auto">
-<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
-<br/><small><?php 	echo $row['title'] ; ?></small></h6>
-</div>
+<div class="tab-content following"><!-- start for both-->
 
-</div>
-<hr />
+
+
+<div class="tab-pane active" id="follows" role="tabpanel"><!-- start following you-->
+
+
+
+  <?php while($row = $result->fetch_assoc()){
+
+
+
+ if (in_array($row['user_id'], $following_me_users)) {?>
+
+
+
+   <div class="row">
+
+     <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+
+   <img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+
+
+	<h6><?php echo $row['first_name'] . $row['last_name']; ?>
+
+
+
+   <br/><small><?php 	echo $row['title']; ?></small></h6>
+
+     </div>
+
+   </div>
+
+  <hr />
+
+ <?php } ?>
+
+
+
 <?php } ?>
-</div>
-</div>
-</div>
-</div>
+
+
+
+
+
 </div> <!-- end following you-->
 
 
