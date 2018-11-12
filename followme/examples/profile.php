7d952395c243f647eeecac780c79afd89d90a70c
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:49:11 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -25,10 +25,8 @@ $following_user_id[] = $row[0];
 $sqlme = "SELECT fm_user_id FROM fm_followers WHERE following_user_id = $userID";
 $following_me = $conn->query($sqlme);
 
-
-
 while ($row = $following_me->fetch_row()) {
-$following_me_me[] = $row[0];
+$following_me[] = $row[0];
 
 }
 
@@ -135,32 +133,26 @@ $following_me_me[] = $row[0];
 </div>
 
 <!-- Tab panes -->
-<div class="tab-content following">
-<div class="tab-pane active" id="follows" role="tabpanel">
-<div class="row">
-<div class="col-md-6 ml-auto mr-auto">
-<ul class="list-unstyled follows">
-<li>
+
+<div class="tab-pane text-center" id="following" role="tabpanel">
+<?php while($row = $result->fetch_assoc()){ 
+  if (in_array($row['user_id'], $following_me))?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-
-<?php while($row = $result->fetch_assoc()){
-if (in_array($row['user_id'], $following_me_me)) {?>
+<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
 <div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-<h6><?php echo $row['first_name'] . $row['last_name']; ?>
-<br/><small><?php 	echo $row['title']; ?></small></h6>
+<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+<br/><small><?php 	echo $row['title'] ; ?></small></h6>
 </div>
 </div>
-</li>
 <hr />
-
-</ul>
+<?php } ?>
 </div>
 </div>
 </div>
-
+</div>
+</div>
 
 
 
