bff5101ab616fdd16425ed5b33f17a4d6ed05a21
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:58:06 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -178,8 +178,8 @@ $following_me[] = $row[0];
 
 <!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
-<?php while($row2 = $result->fetch_assoc()){ 
-  if (in_array($row2['user_id'], $following_user_id))?>
+<?php while($row = $result->fetch_assoc()){ 
+  if (in_array($row['user_id'], $following_user_id))?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
