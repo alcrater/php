5079a601e228094294e964801e1af1ce8011adbe
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:22:08 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -9,9 +9,21 @@ $result = $conn->query($sql);
 
 $user_id = $_SESSION['user_id'];
 
-$sql = "SELECT following_user_id FROM fm_followers WHERE fm_user_id = '$user_id'";
+//follow me sql // change few id's from the following sql statement below
 
-$follow_result = $conn->query($sql);
+$sql44 = "SELECT fm_user_id FROM fm_follows WHERE following_user_id = $userID";
+$following_me = $conn->query($sql44);
+
+while ($row = $following_me->fetch_row()) {
+ $following_me[] = $row[0];
+
+}
+
+
+//following sql
+
+$sql22 = "SELECT following_user_id FROM fm_followers WHERE fm_user_id = '$user_id'";
+$follow_result = $conn->query($sql22);
 
 while($row = $follow_result->fetch_row()) {
 $following_user_id[] = $row[0];
@@ -120,55 +132,31 @@ $following_user_id[] = $row[0];
 </div>
 
 <!-- Tab panes -->
+<!--ADD Follow section php-->
 <div class="tab-content following">
 <div class="tab-pane active" id="follows" role="tabpanel">
-<div class="row">
-<div class="col-md-6 ml-auto mr-auto">
-<ul class="list-unstyled follows">
-<li>
+<?php while($row = $result->fetch_assoc()){
+if (in_array($row['user_id'], $following_me)) {?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-<img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
 <div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6>Flume<br/><small>Musical Producer</small></h6>
-</div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="" checked>
-<span class="form-check-sign"></span>
-</label>
+<h6><?php echo $row['first_name'] . $row['last_name'];?><br/>
+<small><?php 	echo $row['title']; ?></small></h6>
 </div>
 </div>
-</div>
-</li>
 <hr />
-<li>
-<div class="row">
-<div class="col-md-2 ml-auto mr-auto ">
-<img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4">
-<h6>Banks<br /><small>Singer</small></h6>
-</div>
-<div class="col-md-3 col-sm-2">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="">
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
-</div>
-</li>
-</ul>
-</div>
-</div>
-</div>
+<?php } ?>
+<?php } ?>
+
+
+
+
 <!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
-<?php while($row = $result->fetch_assoc()){ ?>
+<?php while($row = $result->fetch_assoc()){
+   if (in_array($row2['user_id'], $following_user_id)) {?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
@@ -177,22 +165,11 @@ $following_user_id[] = $row[0];
 <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
 <br/><small><?php 	echo $row['title'] ; ?></small></h6>
 </div>
-<div class="col-md-3 col-sm-2  ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label"><!--echo if checked only if followed -->
-<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
 </div>
 <hr />
 <?php } ?>
-</div>
-</div>
-</div>
-</div>
-</div>
+<?php } ?>
+
 <footer class="footer section-dark">
 <div class="container">
 <div class="row">
