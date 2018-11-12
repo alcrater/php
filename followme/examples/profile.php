c573c1ce9a4828646a1da436745844711fdbc0f2
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 15:28:25 -0500

    update to profile

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -180,7 +180,7 @@ $following_user_id[] = $row[0];
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
-<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+<div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
 <br/><small><?php 	echo $row['title'] ; ?></small></h6>
 </div>
