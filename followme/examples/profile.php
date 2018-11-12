4e472bcbe13d7dd7ba9d81b7f0dbc016be4cbd25
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-12 16:13:02 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -175,7 +175,7 @@ $following_user_id[] = $row[0];
 <!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
 <?php while($row = $result->fetch_assoc()){ 
-  if (in_array($row['user_id'], $following_user_id)){echo "checked";}?>
+  if (in_array($row['user_id'], $following_user_id))?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
