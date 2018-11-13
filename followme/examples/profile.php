ddd2c1afe0f075d5d5390125882180a3561b598f
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 10:45:01 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -45,7 +45,9 @@ $following_user_id[] = $row1[0];
 <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
 <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<title>Users</title>
+
+<title>Profile Page</title>
+
 <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
 <meta name="viewport" content="width=device-width" />
 
@@ -63,95 +65,156 @@ $following_user_id[] = $row1[0];
 </head>
 
 <body>
+  <!--Navigation Bar-->
   <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
     <div class="container">
-			<div class="navbar-translate">
-	       <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-			     <span class="navbar-toggler-bar"></span>
-				   <span class="navbar-toggler-bar"></span>
-				   <span class="navbar-toggler-bar"></span>
-	       </button>
-	       <a class="navbar-brand" href="#">Users</a>
-			</div>
-			<div class="collapse navbar-collapse" id="navbarToggler">
-	            <ul class="navbar-nav ml-auto">
-	                <li class="nav-item">
-	                    <a href="login.php" class="nav-link">Login</a>
-	                </li>
-                  <li class="nav-item">
-	                    <a href="profile.php" class="nav-link">Profile</a>
-	                </li>
-                  <li class="nav-item">
-	                    <a href="editprofile.php" class="nav-link">Edit Profile</a>
-	                </li>
-                    <li class="nav-item">
-                        <a href="users_old.php" class="nav-link">Users</a>
-                    </li>
-                    <li class="nav-item">
-	                    <a href="#" class="nav-link">
-				<?php echo $_SESSION['email']; ?>
-											</a>
-	                </li>
-	            </ul>
-	        </div>
-		</div>
-    </nav>
+	<div class="navbar-translate">
+	<button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+	<span class="navbar-toggler-bar"></span>
+	<span class="navbar-toggler-bar"></span>
+	<span class="navbar-toggler-bar"></span>
+	</button>
+	 <a class="navbar-brand" href="#">Edit Profile</a>
+	</div>
+	<div class="collapse navbar-collapse" id="navbarToggler">
+	 <ul class="navbar-nav ml-auto">
+     <li class="nav-item">
+     <a href="login.php" class="nav-link">Login</a>
+     </li>
+     <li class="nav-item">
+     <a href="profile.php" class="nav-link">Profile</a>
+     </li>
+     <li class="nav-item">
+     <a href="editprofile.php" class="nav-link">Edit Profile</a>
+    </li>
+    <li class="nav-item">
+     <a href="users_old.php" class="nav-link">Users</a>
+     </li>
+     <li class="nav-item">
+    <a href="#" class="nav-link">
+      <?php echo $_SESSION['email']; ?>
+     </a>
+    </li>
+     </li>
+	 </ul>
+	 </div>
+	</div>
+  </nav> <!-- nav bar end-->
 
-    <div class="wrapper">
-      <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
-			  <div class="filter"></div>
-		  </div>~
+<div class="wrapper">
+<div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+<div class="filter">
+</div>
+</div>
+<div class="section profile-content">
+<div class="container">
+<div class="owner">
+<div class="avatar">
+<img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+</div>
+<div class="name">
+<h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4>
+<h6 class="description"><?php echo $_SESSION['title']; ?></h6>
+</div>
+</div>
+<div class="row">
+<div class="col-md-6 ml-auto mr-auto text-center">
+<p><?php echo $_SESSION['description']; ?></p>
+<br />
+<btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
+</div>
+</div>
+<br/>
+<div class="nav-tabs-navigation">
+<div class="nav-tabs-wrapper">
+<ul class="nav nav-tabs" role="tablist">
+<li class="nav-item">
+<a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
+</li>
+</ul>
+</div>
+</div>
 
-			<br />
-			<br />
-
-    <form action="" method="POST">
-      
-			<div class="row">
-				<div class="col-md-6 ml-auto mr-auto">
-					<ul class="list-unstyled follows">
-						 <?php while($row2 = $result->fetch_assoc()){
-               if (in_array($row2['user_id'], $following_user_id)){ ?>
-						<li>
-							<div class="row">
-								<div class="col-md-2 col-sm-2 ml-auto mr-auto">
-							<!-- image-->	<img src="<?php  echo  $row2['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-								</div>
-								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
-							<!--name-->		<h6><?php echo $row2['first_name'] ." " . $row2['last_name'] ; ?>
-
-							<!-- title-->	<br/><small><?php 	echo $row2['title'] ; ?></small></h6>
-								</div>
-					
-							</div>
-						</li>
-						<hr />
-					<?php } ?>
-					</ul>
-				</div>
-			</div>
-
-
-      <div class="row">
-          <div class="col-md-4 ml-auto mr-auto text-center">
-              <button class="btn btn-danger btn-lg btn-fill">Submit</button>
-          </div>
-      </div>
-
-    </form>
-		</div>
-
-	<footer class="footer section-dark">
-        <div class="container">
+<!-- Tab panes followers-->
+<div class="tab-content following">
+    <div class="tab-pane active" id="follows" role="tabpanel">
+          <?php while($row = $result->fetch_assoc()){
+          if (in_array($row['user_id'], $following_me)) {?>
+       <div class="row">
+          <div class="col-md-6 ml-auto mr-auto">
             <div class="row">
-                <nav class="footer-nav">
-                    <ul>
-                        <li><a href="https://www.creative-tim.com">Creative Tim</a></li>
-                        <li><a href="http://blog.creative-tim.com">Blog</a></li>
-                        <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
-                    </ul>
-                </nav>
-                <div class="credits ml-auto">
-                    <span class="copyright">
-                        © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
-                        </html>
+              <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+                <img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+               </div>
+                      <div class="col-md-7 col-sm-4 ml-auto mr-auto">
+                        <h6><?php echo $row['first_name'] . $row['last_name']; ?><br />
+                        <small><?php 	echo $row['title']; ?></small></h6>
+                      </div><!--end div class-->
+            </div><!--end div class-->      
+          </div><!--end div row-->
+        </div><!--end div row-->
+        <hr />
+        <?php } ?>
+       <?php } ?>
+    </div> <!--end main div-->
+</div> <!--end main div-->
+
+
+
+
+
+<!--Following Section from Users Old-->
+<div class="tab-pane text-center" id="following" role="tabpanel">
+      <?php while($row2 = $result2->fetch_assoc()){
+      if (in_array($row2['user_id'], $following_user_id)) {?>
+<div class="row">
+          <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+          <img src="<?php  echo  $row2['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+          </div><!--//end div class-->
+                    <div class="col-md-7 col-sm-4  ml-auto mr-auto">
+                    <h6><?php echo $row2['first_name'] ." " . $row2['last_name'] ; ?>
+                    <br/><small><?php 	echo $row2['title'] ; ?></small></h6>
+                    </div><!--//end div class-->
+      </div><!--//end div row-->
+    <?php } ?>
+    <?php } ?>
+    </div><!--//end div class-->
+</div><!--//end main class-->
+</div><!--//end main class-->
+</div><!--//end main class-->
+
+<footer class="footer section-dark">
+<div class="container">
+<div class="row">
+<nav class="footer-nav">
+<ul>
+<li><a href="https://www.creative-tim.com">Creative Tim</a></li>
+<li><a href="http://blog.creative-tim.com">Blog</a></li>
+<li><a href="https://www.creative-tim.com/license">Licenses</a></li>
+</ul>
+</nav>
+<div class="credits ml-auto">
+<span class="copyright">
+© <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Creative Tem
+</span>
+</div>
+</div>
+</div>
+</footer>
+</body>
+
+<!-- Core JS Files -->
+<script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+<script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
+<!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!-- Paper Kit Initialization snd functons -->
+<script src="../assets/js/paper-kit.js?v=2.1.0"></script>
+
+</html>
