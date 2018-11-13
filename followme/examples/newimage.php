6cbb09a7e90c247a20b80dd7d18c3caf586c9fe9
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 12:38:39 -0500

    update

--- followme/examples/newimage.php
+++ followme/examples/newimage.php
@@ -115,3 +115,251 @@ if ($uploadVerify) {
 }
 
  ?>
+
+ <!doctype html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+
+
+	<title>by chaos</title>
+
+
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <meta name="viewport" content="width=device-width" />
+
+
+
+	<!-- Bootstrap core CSS     -->
+
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+
+	<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+
+
+
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+
+	<link href="../assets/css/demo.css" rel="stylesheet" />
+
+
+
+    <!--     Fonts and icons     -->
+
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+	<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+
+        <div class="container">
+
+			<div class="navbar-translate">
+
+	            <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+
+					<span class="navbar-toggler-bar"></span>
+
+					<span class="navbar-toggler-bar"></span>
+
+					<span class="navbar-toggler-bar"></span>
+
+	            </button>
+
+	            <a class="navbar-brand" href="#">Edit Picture</a>
+
+			</div>
+
+			<div class="collapse navbar-collapse" id="navbarToggler">
+
+	            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item">
+
+                   <a href="logout.php" class="nav-link">Logout</a>
+
+               </li>
+
+                <li class="nav-item">
+
+                   <a href="profile.php" class="nav-link">Profile</a>
+
+               </li>
+
+                <li class="nav-item">
+
+                   <a href="editprofile.php" class="nav-link">Edit Profile</a>
+
+               </li>
+
+                  <li class="nav-item">
+
+                      <a href="users.php" class="nav-link">Users</a>
+
+                  </li>
+
+                  <li class="nav-item">
+
+                   <a href="#" class="nav-link">
+
+                     <?php echo $_SESSION['email']; ?>
+
+                   </a>
+
+               </li>
+
+                  </li>
+
+	            </ul>
+
+	        </div>
+
+		</div>
+
+  </nav> <!-- nav bar end-->
+
+
+
+    <div class="wrapper">
+
+        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+
+			<div class="filter"></div>
+
+		</div>
+
+    <!-- pasted in here-->
+
+
+
+    <div class="section landing-section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-8 ml-auto mr-auto">
+
+                    <h2 class="text-center">Image Upload</h2> <!-- turned to edit profil -->
+
+                    <form class="contact-form" action="" method="POST"> <!-- added in action and method-->
+
+                        <div class="row"><!--starts row-->
+
+
+
+                        <input type="file" name="upload">
+
+                        <br />
+
+                        <br />
+
+
+
+                        </div><!-- ends row-->
+
+
+
+                        <div class="row">
+
+                          <button class="btn btn-lg btn-fill">Submit</button>
+
+                        </div>
+
+
+
+                    </form>
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    </div>
+
+	<footer class="footer section-dark">
+
+        <div class="container">
+
+            <div class="row">
+
+                <nav class="footer-nav">
+
+                    <ul>
+
+                        <li><a href="https://www.creative-tim.com">Creative Tim</a></li>
+
+                        <li><a href="http://blog.creative-tim.com">Blog</a></li>
+
+                        <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
+
+                    </ul>
+
+                </nav>
+
+                <div class="credits ml-auto">
+
+                    <span class="copyright">
+
+                        © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
+
+                    </span>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+
+
+
+<!-- Core JS Files -->
+
+<script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+
+<script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
+<!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+
+
+
+
+<!--  Paper Kit Initialization snd functons -->
+
+<script src="../assets/js/paper-kit.js?v=2.1.0"></script>
