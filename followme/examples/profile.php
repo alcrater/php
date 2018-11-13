0426527b16afac0bf33471d4a0de85173d0327f7
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 10:47:32 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -167,24 +167,42 @@ $following_user_id[] = $row1[0];
 
 
 <!--Following Section from Users Old-->
-<div class="tab-pane text-center" id="following" role="tabpanel">
-      <?php while($row2 = $result2->fetch_assoc()){
-      if (in_array($row2['user_id'], $following_user_id)) {?>
 <div class="row">
-          <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-          <img src="<?php  echo  $row2['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-          </div><!--//end div class-->
-                    <div class="col-md-7 col-sm-4  ml-auto mr-auto">
-                    <h6><?php echo $row2['first_name'] ." " . $row2['last_name'] ; ?>
-                    <br/><small><?php 	echo $row2['title'] ; ?></small></h6>
-                    </div><!--//end div class-->
-      </div><!--//end div row-->
-    <?php } ?>
-    <?php } ?>
-    </div><!--//end div class-->
-</div><!--//end main class-->
-</div><!--//end main class-->
-</div><!--//end main class-->
+<div class="col-md-6 ml-auto mr-auto">
+<ul class="list-unstyled follows">
+<?php while($row = $result->fetch_assoc()){ ?>
+<li>
+<div class="row">
+<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<!-- image--> <img src="<?php echo $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+</div>
+<div class="col-md-7 col-sm-4 ml-auto mr-auto">
+<!--name--> <h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+<!-- title--> <br/><small><?php echo $row['title'] ; ?></small></h6>
+</div>
+<div class="col-md-3 col-sm-2 ml-auto mr-auto">
+<div class="form-check">
+<label class="form-check-label"><!--echo if checked only if followed -->
+<input class="form-check-input" type="checkbox" name="<?php echo $row['user_id'];?>" value="yes" <?php if (in_array($row['user_id'], $following_user_id)){echo "checked";}?> >
+<span class="form-check-sign"></span>
+</label>
+</div>
+</div>
+</div>
+</li>
+<hr />
+<?php } ?>
+</ul>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-4 ml-auto mr-auto text-center">
+<button class="btn btn-danger btn-lg btn-fill">Submit</button>
+</div>
+</div>
+</form>
+</div>
 
 <footer class="footer section-dark">
 <div class="container">
