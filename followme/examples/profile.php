ac81a1d78aa28dab259b738cbbd8cb4eedf56dda
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 11:53:09 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -108,179 +108,80 @@ $following_user_id[] = $row1[0];
 <div class="filter"></div>
 
 </div>
-
 <div class="section profile-content">
-
-    <div class="container">
-
-        <div class="owner">
-
-            <div class="avatar">
-
-                <img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive"><!-- image, replaced source-->
-
-            </div>
-
-            <div class="name">
-
-                <h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4><!-- first name last name -->
-
-    <h6 class="description"><?php echo $_SESSION['title']; ?></h6> <!-- title-->
-
-            </div>
-
-        </div>
-
-        <div class="row">
-
-            <div class="col-md-6 ml-auto mr-auto text-center">
-
-                <p><?php echo $_SESSION['description']; ?> </p> <!-- description-->
-
-                <br />
-
-                <btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
-
-            </div>
-
-        </div>
-
-        <br/>
-
-        <div class="nav-tabs-navigation">
-
-            <div class="nav-tabs-wrapper">
-
-                <ul class="nav nav-tabs" role="tablist">
-
-                    <li class="nav-item">
-
-                        <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
-
-                    </li>
-
-                    <li class="nav-item">
-
-                        <a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
-
-                    </li>
-
-                </ul>
-
-            </div>
-
-        </div>
-
-
-
-        <!-- Tab panes --> <!-- on profile make following show list of following, dont need checkboxes-->
-
-        <div class="tab-content following"><!-- start for both-->
-
-
-
-            <div class="tab-pane active" id="follows" role="tabpanel"><!-- start following you-->
-
-
-
-              <?php while($row = $result->fetch_assoc()){
-
-
-
-             if (in_array($row['user_id'], $following_me)) {?>
-
-
-
-               <div class="row">
-
-                 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-
-                 <!-- image-->	<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-
-
-
-               <!--name-->		<h6><?php echo $row['first_name'] . $row['last_name']; ?>
-
-
-
-               <!-- title-->	<br/><small><?php 	echo $row['title']; ?></small></h6>
-
-                 </div>
-
-               </div>
-
-              <hr />
-
-             <?php } ?>
-
-
-
-           <?php } ?>
-
-
-
-
-
-            </div> <!-- end following you-->
-
-
-
-            <div class="tab-pane text-center" id="following" role="tabpanel"><!-- list of you following-->
-
-
-
-              <?php while($row2 = $result2->fetch_assoc()){
-
-
-
-             if (in_array($row2['user_id'], $following_user_id)) {?>
-
-
-
-               <div class="row">
-
-                 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-
-                 <!-- image-->	<img src="<?php  echo  $row2['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-
-
-
-               <!--name-->		<h6><?php echo $row2['first_name'] . $row2['last_name']; ?>
-
-
-
-               <!-- title-->	<br/><small><?php 	echo $row2['title']; ?></small></h6>
-
-
-
-
-
-                 </div>
-
-               </div>
-
-              <hr />
-
-             <?php } ?>
-
-
-
-           <?php } ?>
-
-
-
-         </div><!-- end following-->
-
-
-
-        </div><!-- end of following both-->
-
-
-
-    </div>
-
+<div class="container">
+<div class="owner">
+ <div class="avatar">
+<img src="<?php echo $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+ </div>
+ <div class="name">
+<h4 class="title"><?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name']; ?><br /></h4>
+<h6 class="description"><?php echo $_SESSION['title']; ?></h6> 
 </div>
+</div>
+<div class="row">
 
+<div class="col-md-6 ml-auto mr-auto text-center">
+<p><?php echo $_SESSION['description']; ?> </p> 
+<br />
+<btn class="btn btn-outline-default btn-round"><i class="fa fa-cog"></i> Settings</btn>
+</div>
+</div>
+ <br/>
+ <div class="nav-tabs-navigation">
+<div class="nav-tabs-wrapper">
+<ul class="nav nav-tabs" role="tablist">
+<li class="nav-item">
+ <a class="nav-link active" data-toggle="tab" href="#follows" role="tab">Follows</a>
+ </li>
+
+ <li class="nav-item">
+<a class="nav-link" data-toggle="tab" href="#following" role="tab">Following</a>
+</li>
+ </ul>
+</div>
+</div>
+   
+
+<div class="tab-content following"><!-- start for both-->
+
+<!-- start following me-->
+ <div class="tab-pane active" id="follows" role="tabpanel">
+
+<?php while($row = $result->fetch_assoc()){
+ if (in_array($row['user_id'], $following_me)) {?>
+<div class="row">
+  <div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<h6><?php echo $row['first_name'] . $row['last_name']; ?>
+<br/><small><?php 	echo $row['title']; ?></small></h6>
+</div>
+</div>
+ <hr />
+<?php } ?>
+<?php } ?>
+</div> <!-- end following me-->
+
+
+<!--following users I'm following-->
+<div class="tab-pane text-center" id="following" role="tabpanel">
+<?php while($row1 = $result1->fetch_assoc()){
+if (in_array($row1['user_id'], $following_user_id)) {?>
+<div class="row">
+<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<img src="<?php  echo  $row1['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<h6><?php echo $row2['first_name'] . $row2['last_name']; ?>
+<br/><small><?php 	echo $row2['title']; ?></small></h6>
+</div>
+ </div>
+<hr />
+ <?php } ?>
+ <?php } ?>
+ </div><!-- end following-->
+
+</div><!-- end of following both-->
+</div>
+</div>
 </div>
 
 <footer class="footer section-dark">
