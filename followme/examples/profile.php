a07278e642e0b6722213460189f5c201dab41e72
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 08:04:29 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -138,16 +138,19 @@ $following_me[] = $row[0];
 <!-- Tab panes -->
 <div class="tab-content following">
 <div class="tab-pane active" id="follows" role="tabpanel">
+<?php while($row = $result->fetch_assoc()){
+if (in_array($row['user_id'], $following_me)) {?>
 <div class="row">
 <div class="col-md-6 ml-auto mr-auto">
 <ul class="list-unstyled follows">
 <li>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-<img src="../assets/img/faces/clem-onojeghuo-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
 <div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6>Flume<br/><small>Musical Producer</small></h6>
+<h6><?php echo $row['first_name'] . $row['last_name']; ?><br />
+<small><?php 	echo $row['title']; ?></small></h6>
 </div>
 <div class="col-md-3 col-sm-2 ml-auto mr-auto">
 <div class="form-check">
@@ -182,6 +185,7 @@ $following_me[] = $row[0];
 </div>
 </div>
 </div>
+
 
 
 
