2944b30b320ab5782450a65c716a0f7631f98e8c
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 10:16:39 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -140,23 +140,21 @@ $following_user_id[] = $row1[0];
 
 <!-- Tab panes followers-->
 <div class="tab-content following">
-  <div class="tab-pane active" id="follows" role="tabpanel">
-        <?php while($row1 = $result->fetch_assoc()){
-        if (in_array($row1['user_id'], $following_me)) {?>
-        <div class="row">
-        <div class="col-md-6 ml-auto mr-auto">
-        <ul class="list-unstyled follows">
-        <li>
+<div class="tab-pane active" id="follows" role="tabpanel">
+<?php while($row1 = $result->fetch_assoc()){
+if (in_array($row1['user_id'], $following_me)) {?>
 <div class="row">
-    <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-    <img src="<?php  echo  $row1['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-    </div>
-            <div class="col-md-7 col-sm-4 ml-auto mr-auto">
-            <h6><?php echo $row1['first_name'] . $row1['last_name']; ?><br />
-            <small><?php 	echo $row1['title']; ?></small></h6>
-            </div>
-            <?php } ?>
-            <?php } ?>
+<div class="col-md-6 ml-auto mr-auto">
+<ul class="list-unstyled follows">
+<li>
+<div class="row">
+<div class="col-md-2 col-sm-2 ml-auto mr-auto">
+<img src="<?php  echo  $row1['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+</div>
+<div class="col-md-7 col-sm-4 ml-auto mr-auto">
+<h6><?php echo $row1['first_name'] . $row1['last_name']; ?><br />
+<small><?php 	echo $row1['title']; ?></small></h6>
+</div>
 <div class="col-md-3 col-sm-2 ml-auto mr-auto">
 <div class="form-check">
 <label class="form-check-label">
@@ -165,11 +163,13 @@ $following_user_id[] = $row1[0];
 </label>
 </div>
 </div>
+<?php } ?>
+<?php } ?>
 </div>
 </li>
 <hr />
 <li>
-<!--<div class="row">
+<div class="row">
 <div class="col-md-2 ml-auto mr-auto ">
 <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
@@ -184,7 +184,7 @@ $following_user_id[] = $row1[0];
 </label>
 </div>
 </div>
-</div>-->
+</div>
 </li>
 </ul>
 </div>
@@ -198,7 +198,7 @@ $following_user_id[] = $row1[0];
 <!--Following Section from Users Old-->
 <div class="tab-pane text-center" id="following" role="tabpanel">
 <?php while($row2 = $result2->fetch_assoc()){
-if (in_array($row2['user_id'], $following_user_id)) {?>
+if (in_array($row2['user_id'], $following_user_id)) }?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
 <img src="<?php  echo  $row2['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
