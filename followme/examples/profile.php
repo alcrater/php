0fe470886369ec998e418a5b2e7aeeb9e1b2f6af
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 10:18:45 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -155,37 +155,14 @@ if (in_array($row1['user_id'], $following_me)) {?>
 <h6><?php echo $row1['first_name'] . $row1['last_name']; ?><br />
 <small><?php 	echo $row1['title']; ?></small></h6>
 </div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="" checked>
-<span class="form-check-sign"></span>
-</label>
 </div>
-</div>
+
 <?php } ?>
 <?php } ?>
 </div>
 </li>
 <hr />
-<li>
-<div class="row">
-<div class="col-md-2 ml-auto mr-auto ">
-<img src="../assets/img/faces/ayo-ogunseinde-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
-</div>
-<div class="col-md-7 col-sm-4">
-<h6>Banks<br /><small>Singer</small></h6>
-</div>
-<div class="col-md-3 col-sm-2">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="">
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
-</div>
-</li>
+
 </ul>
 </div>
 </div>
