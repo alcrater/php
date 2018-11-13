7a800bd2310b98b055a8d072b820a7ee15e360a9
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 13:36:50 -0500

    update

--- followme/examples/editprofile.php
+++ followme/examples/editprofile.php
@@ -111,7 +111,7 @@ header('Location: profile.php');
 
 <div class="row">
  <div class="input-group">
-<img src="<?php  echo  $_SESSION['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<img src="<?php  echo  $_SESSION['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 <div class="col-md-6">
  <btn class="btn btn-outline-default btn-round" ><i class="fa fa-cog"></i><a href="newimage.php">Update Profile Image</a></btn>
 </div>
--- followme/examples/users_old.php
+++ followme/examples/users_old.php
@@ -124,12 +124,12 @@ $following_user_id[] = $row[0];
 						<li>
 							<div class="row">
 								<div class="col-md-2 col-sm-2 ml-auto mr-auto">
-							<!-- image-->	<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+							<!-- image-->	<img src="<?php  echo  $row['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 								</div>
 								<div class="col-md-7 col-sm-4  ml-auto mr-auto">
-							<!--name-->		<h6><?php echo $row['first_name'] ." " . $row['last_name'] ; ?>
+							<!--name-->		<h6><?php echo $row['first_name'] ." " . $row['last_name']; ?>
 
-							<!-- title-->	<br/><small><?php 	echo $row['title'] ; ?></small></h6>
+							<!-- title-->	<br/><small><?php 	echo $row['title']; ?></small></h6>
 								</div>
 					<div class="col-md-3 col-sm-2  ml-auto mr-auto">
 									<div class="form-check">
