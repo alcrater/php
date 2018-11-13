82b31e177b6878dcc19029106c736d270ab0716e
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-13 09:41:50 -0500

    update

--- followme/examples/profile.php
+++ followme/examples/profile.php
@@ -137,34 +137,16 @@ $following_me[] = $row1[0];
 </div>
 
 <!-- Tab panes -->
-<div class="tab-content following">
-<div class="tab-pane active" id="follows" role="tabpanel">
-<?php while($row1 = $result1->fetch_assoc()){
-if (in_array($row1['user_id'], $following_user_id)) {?>
-<div class="row">
-<div class="col-md-6 ml-auto mr-auto">
-<ul class="list-unstyled follows">
-<li>
+<div class="tab-pane text-center" id="following" role="tabpanel">
+<?php while($row = $result->fetch_assoc()){
+if (in_array($row['user_id'], $following_me)) }?>
 <div class="row">
 <div class="col-md-2 col-sm-2 ml-auto mr-auto">
-<img src="<?php  echo  $row1['image_url']; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+<img src="<?php  echo  $row['image_url'] ; ?>" alt="Circle Image" class="img-circle img-no-padding img-responsive">
 </div>
-<div class="col-md-7 col-sm-4 ml-auto mr-auto">
-<h6><?php echo $row1['first_name'] . $row1['last_name']; ?><br />
-<small><?php 	echo $row1['title']; ?></small></h6>
-</div>
-<div class="col-md-3 col-sm-2 ml-auto mr-auto">
-<div class="form-check">
-<label class="form-check-label">
-<input class="form-check-input" type="checkbox" value="" checked>
-<span class="form-check-sign"></span>
-</label>
-</div>
-</div>
-</div>
-</li>
-<hr />
-</ul>
+<div class="col-md-7 col-sm-4  ml-auto mr-auto">
+<h6><?php echo $row2['first_name'] ." " . $row['last_name'] ; ?>
+<br/><small><?php 	echo $row['title'] ; ?></small></h6>
 </div>
 </div>
 </div>
