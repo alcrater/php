81f848ac4646b4e864feaf21f6498361e221ddeb
alcrater <42805962+alcrater@users.noreply.github.com>
2018-11-14 11:37:33 -0500

    update

--- followme/examples/newimage.php
+++ followme/examples/newimage.php
@@ -1,118 +1,267 @@
 <?php
- if (!isset($_SESSION)) {
-     session_start();
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
-
- //if username not in database, will move them to login page
-if (!isset($_SESSION['user_id'])) {
-    //die("Don't even try mate");
-    //before html cant but put at bottom, must be at top
-    //when you use header you need 'location: then where you are going to'
-    header('Location: profile.php'); //if you wanted https address you need full url
-
-   
-  }
-  
- 
-    
-  //takes whatever this is and tells you about it, good for trouble shooting
-  var_dump($_FILES['upload']);
-  
-  echo"<hr />";
-  //post could have been changed from php 5 to 7
-  var_dump($_POST['upload']); //trouble shooting wrong statement
-  
-  //use ctrl / to auto comment by line
-  
-  //code for uploading file, will work after post data is sent
-  if (isset($_FILES['upload']) ){ //could use != null after ] instead of isset
-    //check to if uploads folder exists
-    if (!file_exists("uploads")){
-      //if uploads folder(directory) dose not exist create it
-      mkdir("./uploads");
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
     }
-  
-     if(!file_exists("uploads/" . $_SESSION['user_id'])){
-      mkdir("uploads/" . $_SESSION['user_id'], 0777, true);
-     }
+}
+?> 
 
-    $target_dir = "uploads/" . $_SESSION['user_id'] . "/";
 
-    $target_file = $target_dir . basename($_FILES['upload']['name']);
-  
-  $uploadVerify = true;
-  
-  //lets check to see if the file already exists
-  
-  //variables are global
-  if (file_exists($target_file)) {
-    $uploadVerify = false;
-    $ret = "Sorry file already exists";
-  }
-  
-  //check file for type
-  $file_type = $_FILES['upload']['type'];
-  
-  switch ($file_type) {
-    case 'image/jpeg':
-      $uploadVerify = true;
-      break;
-  
-    case 'image/png':
-      $uploadVerify = true;
-      break;
-  
-    case 'image/gif':
-      $uploadVerify = true;
-      break;
-  
-    case 'application/pdf':
-      $uploadVerify = true;
-      break;
-  
-    default:
-      $uploadVerify = false;
-      $ret = "sorry only jpeg, gif, png, and pdf files allowed";
-      break;
-  }
-  
-  
-  //php has file upload limit of 2mb by default
-  if ($_FILES['upload']['size'] > 1000000 ) {
-    $uploadVerify = false;
-    $ret = "Sorry file too big";
-  }
-  
-  //if set value has value can be used as true w/o conditions
-  if ($uploadVerify) {
-    //moves files
-      move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file);
-  }
-  }
-  
-  
-   ?>
- 
-  <?php
 
-if (isset($_SESSION['user_id'])) {
-  require('profile.php');
 
-  }
 
-  ?>
 
-   Upload your file.
-  <br />
-   <!--info on w3schools-->
-   <form action="" method="post" enctype="multipart/form-data">
-     <input type="file" name="upload">
-   <br />
-   <br />
-   <input type="submit">
-  
-   </form>
-  
-   <h5 style="color:red;">
-     <?php if ($ret) { echo $ret; } ?>
-  </h5>
+ <!doctype html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8" />
+
+	<link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+
+	<link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+
+
+	<title>by chaos</title>
+
+
+
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <meta name="viewport" content="width=device-width" />
+
+
+
+	<!-- Bootstrap core CSS     -->
+
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+
+	<link href="../assets/css/paper-kit.css?v=2.1.0" rel="stylesheet"/>
+
+
+
+	<!--  CSS for Demo Purpose, don't include it in your project     -->
+
+	<link href="../assets/css/demo.css" rel="stylesheet" />
+
+
+
+    <!--     Fonts and icons     -->
+
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+
+	<link href="../assets/css/nucleo-icons.css" rel="stylesheet">
+
+
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="150">
+
+        <div class="container">
+
+			<div class="navbar-translate">
+
+	            <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+
+					<span class="navbar-toggler-bar"></span>
+
+					<span class="navbar-toggler-bar"></span>
+
+					<span class="navbar-toggler-bar"></span>
+
+	            </button>
+
+	            <a class="navbar-brand" href="#">Edit Picture</a>
+
+			</div>
+
+			<div class="collapse navbar-collapse" id="navbarToggler">
+
+	            <ul class="navbar-nav ml-auto">
+
+               
+
+                <li class="nav-item">
+
+                   <a href="profile.php" class="nav-link">Profile</a>
+
+               </li>
+
+                <li class="nav-item">
+
+                   <a href="editprofile.php" class="nav-link">Edit Profile</a>
+
+               </li>
+
+                  <li class="nav-item">
+
+                      <a href="users.php" class="nav-link">Users</a>
+
+                  </li>
+
+                  <li class="nav-item">
+
+                   <a href="#" class="nav-link">
+
+                     <?php echo $_SESSION['email']; ?>
+
+                   </a>
+
+               </li>
+
+                  </li>
+
+	            </ul>
+
+	        </div>
+
+		</div>
+
+  </nav> <!-- nav bar end-->
+
+
+
+    <div class="wrapper">
+
+        <div class="page-header page-header-xs" data-parallax="true" style="background-image: url('../assets/img/fabio-mangione.jpg');">
+
+			<div class="filter"></div>
+
+		</div>
+
+    <!-- pasted in here-->
+
+
+
+    <div class="section landing-section">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-8 ml-auto mr-auto">
+
+                    <h2 class="text-center">New Image Upload</h2> 
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        Select image to upload:
+                        <input type="file" name="fileToUpload" id="fileToUpload">
+                        <input type="submit" value="Upload Image" name="submit">
+                </form>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    </div>
+
+	<footer class="footer section-dark">
+
+        <div class="container">
+
+            <div class="row">
+
+                <nav class="footer-nav">
+
+                    <ul>
+
+                        <li><a href="https://www.creative-tim.com">Creative Tim</a></li>
+
+                        <li><a href="http://blog.creative-tim.com">Blog</a></li>
+
+                        <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
+
+                    </ul>
+
+                </nav>
+
+                <div class="credits ml-auto">
+
+                    <span class="copyright">
+
+                        © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Chaos
+
+                    </span>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </footer>
+
+</body>
+
+
+
+<!-- Core JS Files -->
+
+<script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+
+<script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+
+<!-- <script src="../assets/js/tether.min.js" type="text/javascript"></script> -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+
+
+
+
+<!--  Paper Kit Initialization snd functons -->
+
+<script src="../assets/js/paper-kit.js?v=2.1.0"></script>
